44e5d396e9e7eca56069159ca3534cbc70b5ec8e
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-06 18:16:51 +0600

    Add files via upload

---
+++ 08_C_Constants.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int ItemPrice =500;
+    float shippingCoast =45.78, sum;
+    sum = ItemPrice+shippingCoast;
+    printf("Total :%f\n", sum);
+    return 0;
+}
