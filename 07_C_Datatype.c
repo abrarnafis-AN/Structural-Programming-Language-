5f5c06e6e494a26ff14dec2cf43ccf3556aab541
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-06 18:03:02 +0600

    Add files via upload

---
+++ 07_C_Datatype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+     int studentID = 2025345675;
+    float score = 89.5;
+    char grade = 'A';
+
+    printf("%d\n", studentID);
+    printf("%f\n", score);
+    printf("%c\n", grade);
+    return 0;
+
+}
