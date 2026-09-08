f1099eed737e09ec7a56790a8cb53e6dec532a41
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-08 14:17:30 +0600

    Add files via upload

---
+++ 11_C_arithmetic_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 15;
+    int b = 10;
+    printf("%d\n",a/b);//output =1
+
+    float x =15;
+    float y = 10;
+    printf("%f\n", x/y);//output = 1.500...
+    return 0;
+}
