cdf34ee6358021ab3fb1cbb46acf2f9a8e3819ab
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-08 13:04:54 +0600

    Add files via upload

---
+++ 10_C_Operator(arithmetic).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int A = 15;
+    int B = 10;
+
+    printf("A+B = %d\n", A+B);//25
+    printf("A-b = %d\n", A-B);//5
+    printf("A*B = %d\n", A*B);//150
+    printf("A/B = %d\n", A/B);//1
+    printf("A%b = %d\n", A%B);//5
+
+    int X=5;
+    ++X;
+    printf("%d\n", X);//6
+    --X;
+    printf("%d\n", X);//5
+    return 0;
+
+}
