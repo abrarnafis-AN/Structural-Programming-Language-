a012554743710b2f953ade82e4583a075c051926
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-08 12:36:43 +0600

    Add files via upload

---
+++ 09_C_Operators(Example_01).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int sum1 = 200 + 100;//300
+    int sum2 = 100 + 50;//100+50=150
+    int sum3 = 20 + 30;//20+30=50
+    int sum4 = sum1 + sum2 +sum3;//300+150+50=500
+    printf("sum is :%d", sum4);//output= sum :500
+}
