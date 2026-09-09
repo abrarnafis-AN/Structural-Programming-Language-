7684060630c33ae84fcbad9ded6c4bea3d71303c
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-09 23:56:18 +0600

    Add files via upload

---
+++ 14_C_comparison(1_2_3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x =10;
+    int y = 5;
+    printf("%d\n", x>y); //return 1(true) because 10 is greater than 5
+    printf("%d\n", x<y); // return 0(false) because 10 is greater than 5
+
+    int distance = 20;
+    printf("%d\n", distance >= 20);//return 1
+    printf("%d\n", distance < 20);//return 0
+
+    float height = 6;
+    printf("%d\n", height >=10);//return 0
+    printf("%d\n", height <10);//return 1
+
+}
