0c2ecc52d2f70f086238395a4f54bf73342825b2
Abrar Mohammad Nafis <nafisabrarcox@gmail.com>
2026-09-10 13:10:09 +0600

    Add files via upload

---
+++ 16_C_operator(precedence).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r1 = 5 + 2*5;//5+10
+    int r2 = (5+2)*5;//7*5
+    printf("%d\n", r1);//15
+    printf("%d\n", r2);//35
+
+    int x = 8 - 2 +4;
+    int y = 10-(2+3);
+    printf("%d\n", x);//10
+    printf("%d\n", y);//4
+    return 0;
+}
